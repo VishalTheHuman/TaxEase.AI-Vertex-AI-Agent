49a5b11a139774a907b9631be2c76d5c80c5e63c
VishalTheHuman <vishalatmadurai@outlook.com>
2024-06-02 15:58:29 +0530

    Updated

--- src/backend/retrieve.py
+++ src/backend/retrieve.py
@@ -1,25 +1,57 @@
+from typing import List
 from google.api_core.client_options import ClientOptions
 from google.cloud import discoveryengine_v1 as discoveryengine
 from vertexai.preview.generative_models import (
     GenerativeModel,
     GenerationResponse,
 )
-
+import vertexai
 from fastapi import FastAPI
+import asyncio
 
-PROJECT_ID = "taxeaseai" 
-REGION = "us-central1" 
+PROJECT_ID = "taxeaseai"
+REGION = "us-central1"
+
+MEMORY = []
+
+vertexai.init(project=PROJECT_ID, location=REGION)
 
 model = GenerativeModel("gemini-1.5-pro-001")
+model = model.start_chat()
 app = FastAPI()
 
-def retrieveDocumentData( 
-    search_query: str, 
-    project_id = "taxeaseai", 
-    location=  "us", 
-    engine_id= "696ce1c2-d49f-413a-af3e-beb67e3bdbe5"
-    ):
-    
+INSTRUCTION = """
+GOAL:
+You are a Tax Return Filling Assistant  named TaxEase.AI developed by CTRL + ALT + DEV Team. Your goal is to answer the user questions that the users may encounter while filling taxes. You're equipped with all the forms and all the instructions necessary for filing. 
+
+Follow the instructions to answer all the Queries given : 
+
+    - If the answer is available in the context window, Answer it using the data from the context window else answer it yourself. But stick to instructions. 
+    - Don't mention about the existance of context window. 
+    - Give answer in plain text form. 
+    - Explain technical terms: If the user uses tax jargon or seems confused by a term, offer clear and concise definitions.
+    - Try to present the data in the visually appealing format.
+    - Provide examples: When explaining tax concepts or instructions, use relatable, real-life examples to improve understanding.
+    - Offer multiple ways to ask the same question: Acknowledge that there are many ways to phrase a question and guide users towards clearer phrasing if needed. Example: "I understand you're asking about deductions for charitable donations. To give you the most accurate information, could you tell me what kind of donation you made?"
+    - Ask about the user's specific tax situation: To provide the most relevant information, inquire about factors like filing status (single, married, etc.), dependents, income type, and any unique circumstances that might apply.
+    - Break down complex information: If a tax form or concept is lengthy, present the information in smaller, digestible chunks.
+    - Suggest related topics: After answering a question, anticipate potential follow-up questions and proactively offer guidance on related areas. Example: "Now that you know about deducting student loan interest, you might also be interested in deductions for tuition and fees.
+    - Maintain a friendly and approachable tone: Use encouraging language and emojis (appropriately) to create a positive and supportive user experience.
+    - Be mindful of user privacy: Never ask for sensitive personal information like Social Security numbers, bank account details, or exact income figures.
+    - Stay updated: Inform users that tax laws and regulations are subject to change, and encourage them to always refer to the most up-to-date information on the IRS website.
+    - Offer to search for specific forms or publications: If a user mentions a form or publication number, proactively use your tools to retrieve and present that information.
+    - Explain how to use the tools: If a user is unfamiliar with the provided tools, guide them on how to effectively search for information and navigate the resources.
+    - If you don't know the answer, respond with "I don't know."
+"""
+
+model.send_message(INSTRUCTION)
+
+def retrieveDocumentData(
+    search_query: str,
+    project_id="taxeaseai",
+    location="us",
+    engine_id="696ce1c2-d49f-413a-af3e-beb67e3bdbe5"
+):
     client_options = (
         ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
         if location != "global"
@@ -30,7 +62,6 @@ def retrieveDocumentData(
 
     serving_config = f"projects/{project_id}/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_config"
 
-    
     content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
         snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
             return_snippet=True
@@ -66,39 +97,28 @@ def retrieveDocumentData(
     context = ""
     for x in (response._response.results):
         for y in x.document.derived_struct_data.get("chunks"):
-                context += y.get("content")
+            context += y.get("content")
 
-    
     return context
 
-
 @app.get("/query/{query}")
-def processQuery(query:str):
-    PROMPT = f"""Follow the instructions to answer the Query given : 
+async def processQuery(query: str):
+    global MEMORY
+    if len(MEMORY) > 4:
+        MEMORY.pop(0)
 
-    If the answer is available in the context window, Answer it using the data from the context window else answer it yourself. But stick to instructions. 
-
-    - Don't mention about the existance of context window. 
-    - Give answer in plain text form. 
-    - Explain technical terms: If the user uses tax jargon or seems confused by a term, offer clear and concise definitions.
-    - Try to present the data in the visually appealing format.
-    - Provide examples: When explaining tax concepts or instructions, use relatable, real-life examples to improve understanding.
-    - Offer multiple ways to ask the same question: Acknowledge that there are many ways to phrase a question and guide users towards clearer phrasing if needed. Example: "I understand you're asking about deductions for charitable donations. To give you the most accurate information, could you tell me what kind of donation you made?"
-    - Ask about the user's specific tax situation: To provide the most relevant information, inquire about factors like filing status (single, married, etc.), dependents, income type, and any unique circumstances that might apply.
-    - Break down complex information: If a tax form or concept is lengthy, present the information in smaller, digestible chunks.
-    - Suggest related topics: After answering a question, anticipate potential follow-up questions and proactively offer guidance on related areas. Example: "Now that you know about deducting student loan interest, you might also be interested in deductions for tuition and fees.
-    - Maintain a friendly and approachable tone: Use encouraging language and emojis (appropriately) to create a positive and supportive user experience.
-    - Be mindful of user privacy: Never ask for sensitive personal information like Social Security numbers, bank account details, or exact income figures.
-    - Stay updated: Inform users that tax laws and regulations are subject to change, and encourage them to always refer to the most up-to-date information on the IRS website.
-    - Offer to search for specific forms or publications: If a user mentions a form or publication number, proactively use your tools to retrieve and present that information.
-    - Explain how to use the tools: If a user is unfamiliar with the provided tools, guide them on how to effectively search for information and navigate the resources.
-    - If you don't know the answer, respond with "I don't know."
-
+    context_window =  retrieveDocumentData(query)
+    PROMPT = f"""
+    MEMORY : {MEMORY}
     QUERY : {query}\n
 
-    CONTEXT_WINDOW :{retrieveDocumentData(query)}"""
-    print(len(PROMPT))
-    response = model.generate_content(PROMPT)
+    CONTEXT_WINDOW :{context_window}"""
+
+    response = model.send_message(PROMPT).candidates[0].content.parts[0].text
+    MEMORY.append(f"""
+    USER : {query}
+    ANSWER : {response}
+    """)
     return {
-        "response" : response.candidates[0].content.parts[0].text
+        "response": response
     }
